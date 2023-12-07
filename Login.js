f33cd5d7327d2e84664886988f143760bbd03bcd
vikashyadav123x <153011621+vikashyadav123x@users.noreply.github.com>
2023-12-07 10:07:46 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,13 @@
+// javascript code !
+
+ var x=document.getElementById("one").value;
+ var y=document.getElementById("two").value;
+ if(x==="cse326" && y==="k22pf")
+ {
+    window.alert("Login succesful !");
+ }
+ else{
+    window.alertfunction ;abc(){
+("Login failed !");
+ }
+}
---
+++ singup.js
@@ -0,0 +1,17 @@
+// javascript code
+
+function wap()
+{
+    var x = document.getElementById("one").value;
+    var y = document.getElementById("two").value;
+    var z = document.getElementById("three").value;
+
+    if(y===z)
+    {
+        window.alert("sucessfuly created !");
+    }
+    else{
+        window.alert("Your password should be same");
+    }
+
+}
